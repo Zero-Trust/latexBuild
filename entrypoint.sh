22a2599fd646cbbe3bc9d82397c38f145676029a
Zero-Trust <62760177+Zero-Trust@users.noreply.github.com>
2020-08-16 22:10:37 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ args="${3}"
 
 function setupMakefile() {
     target_basename="${file%.*}"
-    sed -i -e "s@template.pdf@${target_basename}.tex@g" ./Makefile
-    sed -i -e "s@template.tex@${target_basename}.tex@g" ./Makefile
+    sed -i -e "s@template.pdf@${target_basename}.tex@g" /Makefile
+    sed -i -e "s@template.tex@${target_basename}.tex@g" /Makefile
 }
 
 # install make command
@@ -18,7 +18,7 @@ cp -f /.latexmkrc ./
 setupMakefile
 
 echo "++++++++++ Makefile +++++++++++++"
-cat Makefile
+cat /Makefile
 echo "++++++++++ Makefile +++++++++++++"
 
 cp -f /Makefile   ./
